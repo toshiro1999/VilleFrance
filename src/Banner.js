0a5324997ad2f75c4e6539cc525ae30c73816572
toshiro1999 <thierrygovv@hotmail.fr>
2022-06-17 15:33:57 +0200

    Change banner title

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,7 @@
 import './Banner.css'
 
 function Banner() {
-    return <h1 className='vf-title'>Villes de France</h1>
+    return <h1 className='vf-title'>Villes France</h1>
 }
 
 export default Banner
